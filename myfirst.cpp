4e8e93684f612099b4840af870cf69e8db3de3ab
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-04-17 21:54:47 +0800

    配置了编译器并修改了一些bug

--- myfirst.cpp
+++ myfirst.cpp
@@ -4,10 +4,11 @@
 int main()                                      //  function header 函数头
 {                                               //  一个函数体的开始
     using namespace std;                        //  告诉编译器，使用标准命名空间,使定义可见
-    cout << "Hello, world!"                     //  信息
+    cout << "Hello, world!";                    //  信息
     cout << endl;                               //  换行
-    cout << "I'm a C++ program."                //  信息
+    cout << "I'm a C++ program.";               //  信息
     cout << endl;                               //  换行
-    cout << "And goodluck!"                     //  信息
+    cout << "And goodluck!";                    //  信息
+    cout << endl;                               //  换行
     return 0;                                   //  返回值，终止main函数
 }
