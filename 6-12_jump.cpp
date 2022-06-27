c7eaf5d574ab60017481104995b84af077163272
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-06-27 21:39:21 +0800

    test

---
+++ 6-12_jump.cpp
@@ -0,0 +1,12 @@
+// jump.cpp -- using continue and break
+#include <iostream>
+const int ArSize = 80;
+int main()
+{
+    using namespace std;
+    char line[ArSize];
+    int spaces = 0;
+
+    cout << "Enter a line of text:\n";
+    cin.get(line)
+}
