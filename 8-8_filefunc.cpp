c11179c81d3cb23efd65eeeef8af0eaa9ef92314
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-07-20 17:45:54 +0800

    对象，继承和引用，还没看懂

---
+++ 8-8_filefunc.cpp
@@ -0,0 +1,40 @@
+// filefunc.cpp -- function with ostream & parameter
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+void file_it(ostream & os, double fo, const double fe[], int n);
+const int LIMIT = 5;
+int main()
+{
+    ofstream fout;
+    const char * fn = "ep-data.txt";
+    fout.open(fn);
+    if (!fout.is_open())
+    {
+        cout << "Can't open " << fn << ". Bye.\n";
+        exit(EXIT_FAILURE);
+    }
+    double objective;
+    cout << "Enter the focal length of your "
+            "telescope objective in mm: ";
+    cin >> objective;
+    double eps[LIMIT];
+    cout << "Enter the focal lengths, in mm, of " << LIMIT
+         << " eyepieces:\n";
+    for (int i = 0; i < LIMIT; i++)
+    {
+        cout << "Eyepiece #" << i + 1 << ": ";
+        cin >> eps[i];
+    }
+    file_it(fout, objective, eps, LIMIT);
+    file_it(cout, objective, eps, LIMIT);
+    cout << "Done\n";
+    return 0;
+}
+void file_it(ostream & os, double fo, const double fe[], int n)
+{
+    ios_base::fmtflags initial;
+    initial = os.setf(ios_base::fixed); // save initial formatting state
+}
