2333a4d8682d0f6074cf3d79b3718a31e5be8afb
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-07-22 17:32:02 +0800

    在函数输入中使用同一个父类的不同子类的对象，以达到同一个函数实现不同的功能

--- 8-8_filefunc.cpp
+++ 8-8_filefunc.cpp
@@ -37,4 +37,20 @@ void file_it(ostream & os, double fo, const double fe[], int n)
 {
     ios_base::fmtflags initial;
     initial = os.setf(ios_base::fixed); // save initial formatting state
+    os.precision(0);
+    os << "Focal length of objective: " << fo << " mm\n";
+    os.setf(ios::showpoint);
+    os.precision(1);
+    os.width(12);
+    os << "f.1. eyepiece";
+    os.width(15);
+    os << "magnification" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        os.width(12);
+        os << fe[i];
+        os.width(15);
+        os << int (fo/fe[i] + 0.5) << endl;
+    }
+    os.setf(initial);   // restore initial formatting state
 }
